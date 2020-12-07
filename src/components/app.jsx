2173db9e0634fbe1f0edb20418fb2fee219e0e8b
Yx_s <xuan_www@sina.com>
2020-12-07 10:42:13 +0800

    init

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>a</li>
+          <li>b</li>
+        </ul>
+      </div>
+    );
+  }
+}
